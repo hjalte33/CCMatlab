6490029101a43f7a96eb2dad0f9055fc448a36e2
Hjalte Nielsen <hjalte33@gmail.com>
2016-11-03 16:23:41 +0100

    skidtet virker! solve_ikine giver det resultat det skal

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-L1 = Link('d', 25, 'a',   0, 'alpha', (pi/2));
+L1 = Link('d', 25, 'a',   0, 'alpha', (pi/2), 'offset', (pi));
 L2 = Link('d',  0, 'a',  25, 'alpha', 0     , 'offset', (pi/2));
 L3 = Link('d',  0, 'a',  25, 'alpha', 0     );
 tool = [ 0  0 1  0
@@ -7,7 +7,7 @@ tool = [ 0  0 1  0
          0  0 0  1];
      
 CCBot = SerialLink([L1 L2 L3], 'name', 'CC Bot', 'tool', tool)
-pose = solve_ikine(20,20,40)
+pose = solve_ikine(0,-5,50)
 
 CCBot.plot(pose)
 CCBot.fkine(pose)
--- solve_ikine.m
+++ solve_ikine.m
@@ -1,10 +1,10 @@
 function [vector] = myikine (x, y, z2)
-    z = z2 - 25;
+    z = z2 -25;
     d1 = 25;
     d2 = 25;
     dx  = sqrt(x^2+y^2+z^2);
-    th0 = atan(y/x);
-    th1 = (pi/2)-atan2(z/sqrt(x^2+y^2),z)-acos((d1^2+dx^2-d2^2)/(2*d1*dx));
+    th0 = -atan2(y,x);
+    th1 = (pi/2) - atan2(z,sqrt(x^2+y^2))-acos((d1^2+dx^2-d2^2)/(2*d1*dx));
     th2 = pi - acos((d1^2+d2^2-dx^2)/(2*d1*d2));
     
     vector =[th0, th1, th2];
