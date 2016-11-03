fd64be551ea99d60fff2326de8dce3cbe86e6e7e
Hjalte Nielsen <hjalte33@gmail.com>
2016-11-03 15:16:48 +0100

    virker næsten, men resultatet er ikke tilfredsstillende, den giver ikke det den skal

--- main.m
+++ main.m
@@ -1,16 +1,16 @@
 L1 = Link('d', 25, 'a',   0, 'alpha', (pi/2));
-L2 = Link('d',  0, 'a',  20, 'alpha', 0     , 'offset', (pi/2));
-L3 = Link('d',  0, 'a',   0, 'alpha', 0     );
-tool = [ 0  0 1 20
+L2 = Link('d',  0, 'a',  25, 'alpha', 0     , 'offset', (pi/2));
+L3 = Link('d',  0, 'a',  25, 'alpha', 0     );
+tool = [ 0  0 1  0
         -1  0 0  0
          0 -1 0  0
          0  0 0  1];
      
 CCBot = SerialLink([L1 L2 L3], 'name', 'CC Bot', 'tool', tool)
-pose = solve_ikine(14,14,35)
+pose = solve_ikine(20,20,40)
 
-CCBot.plot([ 0.78539816339744830961566084581988, -0.58155227771298526690567914444854, 0.93725816054529012112776308455688])
-%T = CCBot.fkine(pose)
+CCBot.plot(pose)
+CCBot.fkine(pose)
 
 %Q0 =[1 0 0 0
 %     0 1 0 0
--- solve_ikine.m
+++ solve_ikine.m
@@ -1,21 +1,11 @@
-function [vector] = myikine (x, y, z)
-    %syms d1 d2;
-    d1=25;
-    d2=20;
-    syms tth0 tth1 tth2;
-    
-    lx = sqrt(d1^2 + d2^2 - 2*d1*d2*cos(pi-tth2));
-    thx= (((pi-tth2)*d2) / lx)+tth1 ;
-    
-    eqn1 = x == sin(thx)*lx*cos(tth0);
-    eqn2 = y == sin(thx)*lx*sin(tth0);
-    eqn3 = z == cos(thx)*lx
-    
-    sol = solve([eqn1, eqn2, eqn3],[tth0, tth1, tth2]);
- 
-    th0 = sol.tth0;
-    th1 = sol.tth1;
-    th2 = sol.tth2;
+function [vector] = myikine (x, y, z2)
+    z = z2 - 25;
+    d1 = 25;
+    d2 = 25;
+    dx  = sqrt(x^2+y^2+z^2);
+    th0 = atan(y/x);
+    th1 = (pi/2)-atan2(z/sqrt(x^2+y^2),z)-acos((d1^2+dx^2-d2^2)/(2*d1*dx));
+    th2 = pi - acos((d1^2+d2^2-dx^2)/(2*d1*d2));
     
     vector =[th0, th1, th2];
 
